2de5f5ea938786204ffa03a1757a0c5fac5c2a6a
Ibraheem <iysaleh@gmail.com>
2026-04-13 11:33:01 -0700

    testing-version-update

--- python/version.py
+++ python/version.py
@@ -3,7 +3,7 @@ Version tracking for Pickleball Session Manager.
 This is the single source of truth for the application version.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 GITHUB_REPO_OWNER = "iysaleh"
 GITHUB_REPO_NAME = "pickleball_session_manager"
